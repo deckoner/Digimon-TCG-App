4d2f9e6f5f6213637eeac3b1e08d91ed2cb4e91a
deckoner <deckonerDev@gmail.com>
2025-09-13 20:55:40 +0200

    Aux Store

---
+++ src/types/auxItems.ts
@@ -0,0 +1,20 @@
+export interface BT {
+	id: number;
+	name: string;
+	abbreviation: string;
+}
+
+export interface AuxItem {
+	id: number;
+	name: string;
+}
+
+export interface AuxData {
+	bts: BT[];
+	colors: AuxItem[];
+	cardTypes: AuxItem[];
+	rarities: AuxItem[];
+	stages: AuxItem[];
+	attributes: AuxItem[];
+	types: AuxItem[];
+}
